def9478b6702082be86fa125a7f7604c6f181bc0
Jair Trejo <jair@jairtrejo.mx>
2023-01-25 16:40:01 -0800

    4.1 The Ray class

---
+++ ray.js
@@ -0,0 +1,12 @@
+import { mul, add } from "vec3.js";
+
+export class Ray {
+  constructor(origin, direction) {
+    this.origin = origin;
+    this.direction = direction;
+  }
+
+  at(t) {
+    return add(this.origin, mul(t, this.direction));
+  }
+}
